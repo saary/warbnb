632a9d478f3f497ea3d46f831c87b96e70b0e81b
Itay Alperovitch <itaya@Itay-Alperovitch-D47HRY75KK.local>
2023-10-09 17:23:12 +0300

    set google as the only method of logging into the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Nunito } from 'next/font/google'
 
 import Navbar from '@/app/components/navbar/Navbar';
 import LoginModal from '@/app/components/modals/LoginModal';
-import RegisterModal from '@/app/components/modals/RegisterModal';
 import SearchModal from '@/app/components/modals/SearchModal';
 import RentModal from '@/app/components/modals/RentModal';
 
@@ -34,7 +33,6 @@ export default async function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <LoginModal />
-          <RegisterModal />
           <SearchModal />
           <RentModal />
           <Navbar currentUser={currentUser} />
