0bce3b5d6667d4b55556b5e0cd417e0b9ba80c49
Eran Meir <eranmeir@gmail.com>
2023-10-10 23:34:41 +0300

    Fixing UI bug where navbar hides top of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className="pb-20 pt-28">
+        <div className="pb-20 pt-40">
           {children}
         </div>
       </body>
