a94436a6392660eb74bf2dce2c4939baee3254cf
Saar Yahalom <saar@rewire.to>
2023-10-13 17:13:27 +0300

    Add easy navigation between all places and my places

--- app/[lng]/components/Banner.tsx
+++ app/[lng]/components/Banner.tsx
@@ -1,9 +1,13 @@
-type Props = { title: string | undefined; text: string | undefined };
+type Props = {
+  title: string | undefined;
+  text: string | undefined;
+  children?: string | JSX.Element | JSX.Element[] | (() => JSX.Element);
+};
 
-const Banner: React.FC<Props> = ({ title, text }) => {
+const Banner: React.FC<Props> = ({ title, text, children }) => {
   return (
-    <div>
-      <div className="max-w-full rounded overflow-hidden shadow-lg flex justify-center">
+    <div className="max-w-full rounded overflow-hidden shadow-lg justify-center">
+      <div className="max-w-full flex justify-center">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{title}</div>
           {text?.split('\n').map((line, ind) => (
@@ -12,6 +16,11 @@ const Banner: React.FC<Props> = ({ title, text }) => {
             </p>
           ))}
         </div>
+      </div>
+      <div className="max-w-full flex justify-center">
+        <>
+          {children}
+        </>
       </div>
     </div>
   );
--- app/[lng]/components/HomeBanner.tsx
+++ app/[lng]/components/HomeBanner.tsx
@@ -16,7 +16,12 @@ const HomeBanner: React.FC<HomeBannerProps> = ({ isLoggedIn, isHost, name, lng }
   if (isLoggedIn) {
     if (isHost) {
       return (
-        <Banner title={t('homeBannerHostPostLoginTitle')} text={t('homeBannerHostPostLoginText', { name })} />
+        <Banner title={t('homeBannerHostPostLoginTitle')} text={t('homeBannerHostPostLoginText', { name })}>
+            <div className="flex flex-row gap-1 md:gap-0">
+              <a href={`/${lng}/`} className="rounded-lg hover:bg-slate-100 px-3 py-1 mb-2 text-cyan-800 underline">{t('gotoMainPage')}</a>
+              <a href={`/${lng}/properties`} className="rounded-lg hover:bg-slate-100 px-3 py-1 mb-2 text-cyan-800 underline">{t('gotoMyPropertiesPage')}</a>
+            </div>
+        </Banner>
       );
     }
     else {
